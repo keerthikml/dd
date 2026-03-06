fa5bd34ce5bb5e5428f5cb1e0978cf6297bad13b
keerthikml <keerthiksowthadka@gmail.com>
2026-03-06 15:25:24 +0530

    fix unused icons for netlify build

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,11 +1,36 @@
+// import React from "react";
+// import { Col, Row } from "react-bootstrap";
+// import {
+//   SiVisualstudiocode,
+//   SiPostman,
+//   SiSlack,
+//   SiVercel,
+//   SiMacos,
+// } from "react-icons/si";
+
+// function Toolstack() {
+//   return (
+//     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      
+//       <Col xs={4} md={2} className="tech-icons">
+//         <SiVisualstudiocode />
+//       </Col>
+//       <Col xs={4} md={2} className="tech-icons">
+//         <SiPostman />
+//       </Col>
+     
+//     </Row>
+//   );
+// }
+
+// export default Toolstack;
+
+
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
   SiVisualstudiocode,
-  SiPostman,
-  SiSlack,
-  SiVercel,
-  SiMacos,
+  SiPostman
 } from "react-icons/si";
 
 function Toolstack() {
@@ -15,10 +40,11 @@ function Toolstack() {
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <SiPostman />
       </Col>
-     
+
     </Row>
   );
 }
